f4fb58079e9eb5a53435382cf9fef0231c2e251b
chudanhtuan <chudanhtuan2034@gmail.com>
2024-03-13 16:35:32 +0700

    done

---
+++ src/main/java/com/example/practice2/jwt/config/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.example.practice2.jwt.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
